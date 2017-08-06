4be700c4a941bf830494144545d275b8d085d574
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-08-05 20:25:18 -0700

    select camera driver through an environment variable

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+from importlib import import_module
+import os
 from flask import Flask, render_template, Response
 
-# emulated camera
-from camera import Camera
+# import camera driver
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
 
 # Raspberry Pi camera module (requires picamera package)
 # from camera_pi import Camera
