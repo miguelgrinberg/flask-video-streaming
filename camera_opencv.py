e163bbb881dedf888c8398a3bad65ffbf31a7b09
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-08-25 15:31:37 -0700

    specify video source in opencv driver

--- camera_opencv.py
+++ camera_opencv.py
@@ -3,9 +3,15 @@ from base_camera import BaseCamera
 
 
 class Camera(BaseCamera):
+    video_source = 0
+
+    @staticmethod
+    def set_video_source(source):
+        Camera.video_source = source
+
     @staticmethod
     def frames():
-        camera = cv2.VideoCapture(0)
+        camera = cv2.VideoCapture(Camera.video_source)
         if not camera.isOpened():
             raise RuntimeError('Could not start camera.')
 
