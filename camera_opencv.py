dc53de2ea64e1ee4eac16e2a3b52e144c0254de3
141414252525363636 <LEOLAY1984@HOTMAIL.COM>
2021-07-06 05:26:08 -0300

    Revert "Revert "Use OPENCV_CAMERA_SOURCE environment variable to set source""
    This reverts commit 508ac44f7bdb8f75f7d5ea70045508f4e7edcf4d.

--- camera_opencv.py
+++ camera_opencv.py
@@ -1,9 +1,15 @@
+import os
 import cv2
 from base_camera import BaseCamera
 
 
 class Camera(BaseCamera):
     video_source = 0
+
+    def __init__(self):
+        if os.environ.get('OPENCV_CAMERA_SOURCE'):
+            Camera.set_video_source(int(os.environ['OPENCV_CAMERA_SOURCE']))
+        super(Camera, self).__init__()
 
     @staticmethod
     def set_video_source(source):
