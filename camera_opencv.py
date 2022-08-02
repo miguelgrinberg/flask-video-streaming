b26ca9e640dbca7f03a7e03cd789cb591fa582de
Ben <benknight135@gmail.com>
2022-08-02 16:08:11 +0000

    add random image generation mode to opencv camera

--- camera_opencv.py
+++ camera_opencv.py
@@ -1,14 +1,29 @@
 import os
+import numpy as np
 import cv2
 from base_camera import BaseCamera
 
+class VideoCaptureRND():
+    def __init__(self, size=(100, 100, 3)):
+        self._size = size
+
+    def isOpened(self):
+        return True
+
+    def read(self):
+        rnd_img = np.random.randint(255, size=self._size, dtype=np.uint8)
+        return True, rnd_img
 
 class Camera(BaseCamera):
     video_source = 0
 
     def __init__(self):
         if os.environ.get('OPENCV_CAMERA_SOURCE'):
-            Camera.set_video_source(int(os.environ['OPENCV_CAMERA_SOURCE']))
+            video_source = os.environ.get('OPENCV_CAMERA_SOURCE')
+            if not video_source == "rnd":
+                Camera.set_video_source(int(video_source))
+            else:
+                Camera.set_video_source(video_source)
         super(Camera, self).__init__()
 
     @staticmethod
@@ -17,7 +32,10 @@ class Camera(BaseCamera):
 
     @staticmethod
     def frames():
-        camera = cv2.VideoCapture(Camera.video_source)
+        if Camera.video_source == "rnd":
+            camera = VideoCaptureRND(size=(640,640,3))
+        else:
+            camera = cv2.VideoCapture(Camera.video_source)
         if not camera.isOpened():
             raise RuntimeError('Could not start camera.')
 
